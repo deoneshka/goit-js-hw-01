98b9631cb434ad28b6af8bb1ca74aa81e17107d9
deoneshka <oleg.deonega@gmail.com>
2020-12-01 21:57:44 +0200

    Update task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -4,16 +4,14 @@ let pricePerDroid = 3000;
 let totalPrice;
 
 switch (numberOfDroids) {
-  case null: {
+  case null:
     console.log('Отменено пользователем!');
     break;
-  }
     
-  default: {
+  default:
     totalPrice = Number(numberOfDroids) * pricePerDroid;
 
-    if (totalPrice <= credits) {
-      console.log(`Вы купили ${numberOfDroids} дроидов, на счету осталось ${credits - totalPrice} кредитов.`)
-    } else { console.log('Недостаточно средств на счету!') }
-  }
+  if (totalPrice <= credits) {
+    console.log(`Вы купили ${numberOfDroids} дроидов, на счету осталось ${credits - totalPrice} кредитов.`)
+  } else { console.log('Недостаточно средств на счету!') }
 }
